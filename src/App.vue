772a18ca9ce707ee214aeb95bf5d41541cf95c2a
chenxch <chenxch@ffcs.cn>
2020-07-27 19:49:24 +0800

    qiankun

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
+<!--
+ * @Descripttion: 
+ * @Author: chenxch
+ * @Date: 2020-07-27 09:06:47
+--> 
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <header>
+      <nav>
+        <ol>
+          <li v-for="app of apps" :key="app.name">
+            <a @click="goto(app.name, `${app.activeRule}`)">{{app.name}}</a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+    <div id="appContainer" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start } from 'qiankun'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+    name: 'master',
+		data () {
+      return {
+        apps: [
+          { name: 'xc-micro-app', entry: '//localhost:8081', container: '#appContainer', activeRule: '/xc-micro-app' },
+          { name: 'xc-grafana', entry: '//localhost:8082', container: '#appContainer', activeRule: '/xc-grafana' }
+        ]
+      }
+    },
+    created () {
+      if (!window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__) {
+        this.initQiankun()
+      } else {
+        // fix hot-reload
+        location.reload()
+      }
+    },
+		methods: {
+			goto (title, href) {
+				window.history.pushState({}, title, href)
+      },
+      initQiankun () {
+        registerMicroApps(
+          this.apps,
+          {
+            beforeLoad: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('before load', app)
+              }
+            ],
+            beforeMount: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('before mount', app)
+              }
+            ],
+            afterUnmount: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('after unload', app)
+              }
+            ]
+          }
+        )
+
+        setDefaultMountApp('/slave')
+
+        runAfterFirstMounted(() => {
+          // eslint-disable-next-line no-console
+          console.info('first app mounted')
+        })
+
+        start({ prefetch: true })
+      }
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  a {
+    color: #42b983;
+    cursor: pointer;
+  }
+  .appContainer {
+    margin-top: 50px;
+  }
 </style>
